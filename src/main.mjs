11eb89bd822e72745ac1b2626c50fa2593c4c0b8
Andrew Branch <andrew@wheream.io>
2023-05-30 15:32:57 -0700

    Initial commit

---
+++ src/main.mjs
@@ -0,0 +1,2 @@
+import cjs from './cjs.js';
+console.log("Import CJS default from mjs: ", cjs);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+/** @type {import("webpack").Configuration} */
+const config = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+module.exports = config;
